38035050bb89d70f60c26b64e0c69363f8f3e2cd
DESKTOP-QO272N7\huynh <huynhtinnhan2019@gmail.com>
2022-01-21 16:03:54 +0700

    update room again, the next will update login form

--- public/js/script.js
+++ public/js/script.js
@@ -269,3 +269,14 @@ $("#btn-stop-share").click(() => {
     $("#btn-stop-share").hide();
     
 });
+function GetID() {
+    /* Get the text field */
+    var copyText = document.getElementById("IDroom");
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
